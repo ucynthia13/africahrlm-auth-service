737f34eade86636a4a6d274ee190bf12cec6cee0
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-05-03 16:32:18 +0200

    Added hosted frontend endpoint

--- src/main/java/rw/leavemanagement/auth/config/SecurityConfig.java
+++ src/main/java/rw/leavemanagement/auth/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "https://leave-compass-kigali.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "UPDATE", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
